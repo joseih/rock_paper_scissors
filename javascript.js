c8e67ad9404e4559360955ea2e10ab03eb762952
jose <joseih2010@hotmail.com>
2024-02-08 18:41:03 -0300

    Finalizado

--- javascript.js
+++ javascript.js
@@ -30,27 +30,43 @@ function compare() {
 
     if(playerChoice == computerChoice){
         console.log("Draw!");
+        return null
     }
     if(playerChoice == "scissor" && computerChoice == "rock"){
-        console.log("Lose");
+        console.log("Loss! "+playerChoice+" losses to "+ computerChoice);
+        return false
     }
     if(playerChoice == "scissor" && computerChoice == "paper"){
         console.log("Win");
+        return true
     }
 
     if(playerChoice == "rock" && computerChoice == "paper"){
-        console.log("Lose");
+        console.log("Loss! "+playerChoice+" losses to "+ computerChoice);
     }
     if(playerChoice == "rock" && computerChoice == "scissor"){
-        console.log("Win");
+        console.log("Win "+ playerChoice+" win agaist "+computerChoice );
+        return true
+
     }
     if(playerChoice == "paper" && computerChoice == "scissor"){
-        console.log("Lose");
+        console.log("Loss! "+playerChoice+" losses to "+ computerChoice);
+        
     }
     if(playerChoice == "paper" && computerChoice == "rock"){
-        console.log("Win");
+        console.log("Win "+ playerChoice+" win agaist "+computerChoice );
+        return true
     }
 
 
 }
-compare()
+let playerScore = 0, computerScore = 0
+for (let index = 0; index < 5; index++) {
+    let test = compare()
+    if(test){
+        playerScore++
+    }else if(test === false){
+        computerScore++
+    }
+}
+console.log(`Player Score:${playerScore} |  Computer Score: ${computerScore}`)
