3ecfa62ae538fdafc12364475236bc3af2f1dec3
jose <joseih2010@hotmail.com>
2024-02-07 21:56:10 -0300

    Players Logic

---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+player()
+
+
+function name(params) {
+    
+}
+function player() {
+let choice = prompt("Player turn")
+
+switch (choice) {
+    case "scissor":
+        return "scissor"
+        break;
+    case "rock":
+        return "rock"
+        break
+    case "paper":
+        return "paper"    
+        break
+}
+    
+}
+function computer(params) {
+    
+}
